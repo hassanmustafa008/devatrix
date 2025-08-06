47378a115900af5e0ea6c2b67cdf8c67c30f5ad2
hassanmustafa008 <hassanmustafa008@gmail.com>
2025-08-06 19:42:21 +0500

    update hero section text and fix font-size issue

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -16,31 +16,25 @@ const Hero = () => {
           <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
             <div className=" md:w-1/2">
               <h4 className="mb-4.5 text-lg font-medium text-black dark:text-white">
-                🔥 Solid - A Complete SaaS Web Template
+                🔥 Devatrix - Your Trusted Partner for AI-Powered Solutions
               </h4>
-              <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
-                Free Next.js Template for {"   "}
-                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
-                  SaaS
-                </span>
+              <h1 className="mb-5 pr-16 font-bold text-black dark:text-white xl:text-hero ">
+                Empowering Businesses Through Technology
               </h1>
               <p>
-                Solid Pro - Packed with all the key integrations you need for
-                swift SaaS startup launch, including - Auth, Database, Sanity
-                Blog, Essential Components, Pages and More. Built-winth -
-                Next.js 13, React 18 and TypeScript.
+                Devatrix delivers scalable, secure, and intelligent software solutions tailored to meet your evolving needs.
               </p>
 
               <div className="mt-10">
                 <form onSubmit={handleSubmit}>
                   <div className="flex flex-wrap gap-5">
-                    <input
+                    {/* <input
                       value={email}
                       onChange={(e) => setEmail(e.target.value)}
                       type="text"
                       placeholder="Enter your email address"
                       className="rounded-full border border-stroke px-6 py-2.5 shadow-solid-2 focus:border-primary focus:outline-hidden dark:border-strokedark dark:bg-black dark:shadow-none dark:focus:border-primary"
-                    />
+                    /> */}
                     <button
                       aria-label="get started button"
                       className="flex rounded-full bg-black px-7.5 py-2.5 text-white duration-300 ease-in-out hover:bg-blackho dark:bg-btndark dark:hover:bg-blackho"
@@ -50,9 +44,9 @@ const Hero = () => {
                   </div>
                 </form>
 
-                <p className="mt-5 text-black dark:text-white">
+                {/* <p className="mt-5 text-black dark:text-white">
                   Try for free no credit card required.
-                </p>
+                </p> */}
               </div>
             </div>
 
